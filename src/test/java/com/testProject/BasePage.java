106ff931118fcee11137b7eefa1a5b1c7beae7a0
Andy <andy.aka.shaman@gmail.com>
2021-07-11 12:27:07 +0300

    This is DEmo needs

--- src/test/java/com/testProject/BasePage.java
+++ src/test/java/com/testProject/BasePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.PageFactory;
 public abstract class BasePage {
     protected WebDriver driver;
 
+    //This is my comments
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
