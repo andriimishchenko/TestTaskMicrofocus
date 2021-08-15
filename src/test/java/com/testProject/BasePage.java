7945f526d6a53461331393c7b79610d58c0c8be9
Andy <andy.aka.shaman@gmail.com>
2021-08-15 12:53:58 +0300

    QA Start Up needs

--- src/test/java/com/testProject/BasePage.java
+++ src/test/java/com/testProject/BasePage.java
@@ -7,6 +7,7 @@ public abstract class BasePage {
     protected WebDriver driver;
 
     //This is my comments
+    //this is my second comment
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
