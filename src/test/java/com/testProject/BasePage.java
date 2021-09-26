8af84567df896952c54dbb17f31fd0d3b06527b8
Andy <andy.aka.shaman@gmail.com>
2021-09-26 13:37:42 +0300

    Bla-bla-blasdgdfgfghdfghrth

--- src/test/java/com/testProject/BasePage.java
+++ src/test/java/com/testProject/BasePage.java
@@ -8,6 +8,7 @@ public abstract class BasePage {
 
     //This is my comments
     //this is my second comment
+    //this is my comment for QA DEmo needs
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
