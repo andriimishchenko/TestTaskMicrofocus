85449e62452d11fa6961db331540eb8998cbfcf1
Andy <andy.aka.shaman@gmail.com>
2021-11-28 12:17:22 +0200

    Demo of SCV 28.11.2021

--- src/test/java/com/testProject/BasePage.java
+++ src/test/java/com/testProject/BasePage.java
@@ -7,6 +7,7 @@ public abstract class BasePage {
     protected WebDriver driver;
 
     //comments from QA Group 24.10.2021
+    //comments from QA Group 28.11.2021
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
