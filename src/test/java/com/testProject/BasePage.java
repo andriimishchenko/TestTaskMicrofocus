44d70949db917af5f01889063129005680f83d0b
amishchenko <andrii.mishchenko@microfocus.com>
2021-12-26 14:58:26 +0200

    Task 202909 - Demonstrate to new group GIT workflow

--- src/test/java/com/testProject/BasePage.java
+++ src/test/java/com/testProject/BasePage.java
@@ -8,6 +8,7 @@ public abstract class BasePage {
 
     //comments from QA Group 24.10.2021
     //comments from QA Group 28.11.2021
+    //comments from QA Best Group 26.12.2021
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
