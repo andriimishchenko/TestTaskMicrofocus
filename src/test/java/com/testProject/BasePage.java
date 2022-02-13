accf3640f86355d89f7b18fdf42fdbf9058d0402
Andy <andy.aka.shaman@gmail.com>
2022-02-13 11:57:45 +0200

    Changes from QA Group 02.13.2022

--- src/test/java/com/testProject/BasePage.java
+++ src/test/java/com/testProject/BasePage.java
@@ -6,8 +6,7 @@ import org.openqa.selenium.support.PageFactory;
 public abstract class BasePage {
     protected WebDriver driver;
 
-    //comments from QA Group 24.10.2021
-    //comments from QA Group 28.11.2021
+    //demo of GitHub working 02.13.2022
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
